d1ff7b9238c7e03d694e01d680b74d1955b498de
Evan <ejames917@gmail.com>
2024-04-19 17:56:42 -0600

    Baseline Thing Plus-based controller. Cleaned up a lot of code. Finalized proper funcionality of Rx

--- libraries/memory.py
+++ libraries/memory.py
@@ -12,6 +12,7 @@ class MemoryBuffer:
         data = bytearray(size_bytes)
         # Create a memoryview to it, so we can sub-view the memory in the derived classes
         self._data = memoryview(data)
+        self._data_len = 0
 
     def __str__(self) -> str:
         return "Memory buffer"
@@ -47,15 +48,37 @@ class MemoryBuffer:
         gc.enable()
         start = 0
         decoded_chunk = ''
-        if print_len == -1:
-            print_len = len(self._data)
-        while start < print_len:
-            chunk = self._data[start: start+chunk_size]
-            decoded_chunk = bytes(chunk).decode(encoding)
-            stdout.write(decoded_chunk)
-            start += chunk_size
+        if self._data_len == 0:
+            print("This MemoryBuffer is empty!")
+        else:
+            if print_len == -1:
+                print_len = self._data_len
+            while start < print_len:
+                chunk = self._data[start: start+chunk_size]
+                decoded_chunk = bytes(chunk).decode(encoding)
+                stdout.write(decoded_chunk)
+                start += chunk_size
         gc.disable()
         return ''
+    
+    def _read_ascii(self, _msg: str) -> None:
+        """
+        Write an ASCII string to the memory buffer. 
+
+        Args:
+            _msg (str): ASCII string to save to outbox message buffer
+'        """
+        gc.collect()
+        if len(_msg) > len(self._data):
+            print(f"Your message ({len(_msg)} bytes) is larger than the message buffer ({len(self._data)} bytes)\n"\
+                  "The message will be truncated.")
+        # Set the message length
+        self._data_len = len(_msg)
+        for i, char in enumerate(_msg):
+            if i+1 >= len(self._data):
+                break
+            self._data[i] = ord(char)
+        gc.collect()
         
 class AmsatI2CBuffer(MemoryBuffer):
     def __init__(self, size_bytes) -> None:
@@ -63,11 +86,8 @@ class AmsatI2CBuffer(MemoryBuffer):
 
 class OutboxBuffer(MemoryBuffer):
     def __init__(self, size_bytes) -> None:
-        super().__init__(size_bytes+OUTBOX_HEADER_SIZE)
-        # Byte 0 of _data flags whether _msg is ready to send
-        self.msg_ready = memoryview(self._data[:1])
-        # Byte 1-2 of _data stores length of message (in bytes)
-        self._msg_len = memoryview(self._data[1:3])
+        super().__init__(size_bytes)
+        self.msg_ready = False
         # Bytes 3-n of _data store the outgoing message
         self._msg = memoryview(self._data[3:])
 
@@ -76,65 +96,45 @@ class OutboxBuffer(MemoryBuffer):
     
     def __repr__(self) -> str:
         return f"OutboxBuffer: {len(self._msg)} bytes ({len(self._data)} bytes total)\n\
-                    Message length = {self._memoryview_int(self._msg_len)}\n\
-                    Msg_ready = {bool(self.msg_ready[0])}"
+                    Message length = {self._data_len}\n\
+                    Msg_ready = {self.msg_ready}"
     
     def __bool__(self) -> bool:
-        return bool(self.msg_ready[0])
+        return self.msg_ready
     
     def __len__(self) -> int:
         # Return the length of the message
-        return self._memoryview_int(self._msg_len)
-
-    def _read_ascii(self, _msg: str) -> None:
-        """
-        Write an ASCII string to the outbox. Updates 
-
-        Args:
-            _msg (str): ASCII string to save to outbox message buffer
-'        """
-        gc.collect()
-        if len(_msg) > len(self._msg):
-            print(f"Your message ({len(_msg)} bytes) is larger than the message buffer ({len(self._msg)} bytes)\n"\
-                  "The message will be truncated.")
-        # Set the message length
-        length_bytes = len(_msg).to_bytes(2, 'big')
-        for i,byte in enumerate(length_bytes):
-            self._msg_len[i] = byte
-        for i, char in enumerate(_msg):
-            if i+1 >= len(self._msg):
-                break
-            self._msg[i] = ord(char)
-        gc.collect()
+        return self._data_len
 
     def _set_msg_ready(self, ready:bool=True) -> None:
         """
-        Sets the messsage ready flag in the outbox header
+        Sets the messsage ready flag
 
         Args:
             ready (bool, optional): Whether the outbox _msg is ready to send. Defaults to True.
         """
         if ready:
-            self.msg_ready[0] = 1
+            self.msg_ready = True
         else:
-            self.msg_ready[0] = 0
+            self.msg_ready = False
 
 class InboxBuffer(MemoryBuffer):
     def __init__(self, size_bytes) -> None:
-        super().__init__(size_bytes+INBOX_HEADER_SIZE)
-        self.msg_len = 0
-        self.msg_idx = 0
+        super().__init__(size_bytes)
         self.recording = False
 
     def __str__(self) -> str:
-        return self._print_data_ascii(self.msg_len)
+        return self._print_data_ascii()
     
     def __repr__(self) -> str:
         return super().__repr__()
     
+    def __len__(self) -> int:
+        return self._data_len
+    
     def set_recording(self, recording:bool=True) -> None:
         """
-        Set the recording flag in the inbox header
+        Set the recording flag
 
         Args:
             recording (bool, optional): Whether to record incoming data. Defaults to True.
@@ -144,9 +144,3 @@ class InboxBuffer(MemoryBuffer):
         else:
             self.recording = 0
         self.recording = bool(self.recording)
-    
-    def rx_byte(self, byte_int: int) -> None:
-        if self.recording:
-            self._data[self.msg_idx] = byte_int
-            self.msg_idx += 1
-    
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import gc
 
 from utime import sleep_ms, ticks_ms, ticks_diff
+from machine import Pin
+
 from libraries.rgbled import WS2812
 from libraries.gpio import BUTTON_B_PIN, BUTTON_R_PIN, SWITCH_PIN, LED_PIN
 from libraries.linda import Linda
-from machine import Pin
 
 # Pin Setup
 led = Pin(LED_PIN, Pin.OUT)
@@ -12,6 +13,7 @@ button_B = Pin(BUTTON_B_PIN, Pin.IN, pull=Pin.PULL_DOWN)
 button_R = Pin(BUTTON_R_PIN, Pin.IN, pull=Pin.PULL_DOWN)
 switch = Pin(SWITCH_PIN, Pin.IN, pull=Pin.PULL_DOWN)
 
+# Disable automatic garbage collection
 gc.disable()
 
 # Set up builtin neopixel rgbled with default pins
@@ -26,14 +28,12 @@ debounce_delay_ms = 100
 last_B_debounce_time = 0
 last_R_debounce_time = 0
 
+# Top-level LINDA object
 linda = Linda()
 linda.laser._toggle_tx(False)
 
 # Set initial idle state based on toggle switch
-if switch.value():
-    idle = True
-else:
-    idle = False
+idle = bool(switch.value())
 active_tx_rx = False
 
 linda.laser.outbox._read_ascii('When Stubb had departed, Ahab stood for a while leaning over the bulwarks; and then, as had been usual with him of late, calling a sailor of the watch, he sent him below for his ivory stool, and also his pipe. Lighting the pipe at the binnacle lamp and planting the stool on the weather side of the deck, he sat and smoked. \
@@ -41,27 +41,40 @@ linda.laser.outbox._read_ascii('When Stubb had departed, Ahab stood for a while 
     Some moments passed, during which the thick vapor came from his mounth in quick and constant puffs, which blew back again into his face. "How now", he soliloquized at last, withdrawing the tube, "this smoking no longer soothes. Oh, my pipe! hard must it go with me if thy charm be gone! Here have I been unconsciously toiling, not pleasuring- aye, and ignorantly smmoking to the windward all the while; to windward, and with such nervous whiffs, as if, like the dying whale, my final jets were the strongest and the fullest of trouble. What business have I with this pipe? This thing that is meant for sereneness, to send up mild white vapors among mild white hairs, not among torn iron-grey locks like mine. I\'ll smoke no more-"\
     He tossed the still lighted pipe into the sea. The fire hissed in the waves; the same instant the ship shot by the bubble the sinking pipe made. With slouched hat, Ahab lurchingly paced the planks.')
 
-def check_button_presses():
-        global last_B_debounce_time, last_R_debounce_time, active_tx_rx
-        current_B_state = button_B.value()
-        B_state_changed = current_B_state != last_B_state
-        current_R_state = button_R.value()
-        R_state_changed = current_R_state != last_R_state
-        if B_state_changed:  # Button B pressed
-            if current_B_state == 1:
-                if ticks_diff(ticks_ms(), last_B_debounce_time) >= debounce_delay_ms:
-                    last_B_debounce_time = ticks_ms()
-                    if not idle:
-                        linda.laser._toggle_tx(False)
-                        active_tx_rx = True
-        if R_state_changed: # Button R pressed
-            if current_R_state == 1:
-                if ticks_diff(ticks_ms(), last_R_debounce_time) >= debounce_delay_ms: 
-                    last_R_debounce_time = ticks_ms()
-                    if not idle:
-                        linda.laser._toggle_tx(True)
-                        active_tx_rx = True
+def check_button_presses() -> None:
+    """
+    Checks for button presses on Blue and Red buttons, and sets global states accordingly
+    """
+    global last_B_debounce_time, last_R_debounce_time, active_tx_rx
+    current_B_state = button_B.value()
+    B_state_changed = current_B_state != last_B_state
+    current_R_state = button_R.value()
+    R_state_changed = current_R_state != last_R_state
+    if B_state_changed:  # Button B pressed
+        if current_B_state == 1:
+            if ticks_diff(ticks_ms(), last_B_debounce_time) >= debounce_delay_ms:
+                last_B_debounce_time = ticks_ms()
+                if not idle:
+                    linda.laser._toggle_tx(False)
+                    active_tx_rx = True
+    if R_state_changed: # Button R pressed
+        if current_R_state == 1:
+            if ticks_diff(ticks_ms(), last_R_debounce_time) >= debounce_delay_ms: 
+                last_R_debounce_time = ticks_ms()
+                if not idle:
+                    linda.laser._toggle_tx(True)
+                    active_tx_rx = True
 
+# Main functional loop
+# Idle/Active modes are toggled with a hardware switch
+# If LINDA is idle, it will cycle through pretty colors on the builtin Neopixel rgbled
+#    this also acts as an alignment mode, where the attached LED will illuminate on laser detector activity
+# If LINDA is active, it will either transmit or recieve upon Red/Blue button press
+#    Red button press will transmit data from the LindaLaser outbox memory buffer
+#    Blue button press will start the recieve routine which will capture incoming bits and save the resultant
+#        ASCII string to the LindaLaser inbox memory buffer
+# At the end of the main loop, check for button presses or mode switch changes, 
+#    then run garbage collection and take a short rest
 while True:
     if idle:
         linda.laser.laser.on()
@@ -83,8 +96,7 @@ while True:
             else:
                 ws.set_color(0,0,255)
                 led.on()
-                linda.laser.start_rx(duration=2)
-                print('rx')
+                linda.laser.start_rx()
             
             active_tx_rx = False
             ws.set_color(0,255,0)
